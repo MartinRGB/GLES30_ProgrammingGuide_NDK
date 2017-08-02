9468d3361cd5191b710a1816fac8e8fa57e851a2
MartinRGB <qiuyinsen@gmail.com>
2017-08-02 14:35:27 +0800

    add Vertex_MVPMatrix Project
    add Vertex_MVPMatrix Project

---
+++ Chapter8_2_1_VertexShader_MVPMatrix/app/obj/local/arm64-v8a/objs/android_native_app_glue/android_native_app_glue.o.d
@@ -0,0 +1,5 @@
+/Users/MartinRGB/Desktop/Github/GLES30_ProgrammingGuide_NDK/Chapter2_Hello_Triangle/app/obj/local/arm64-v8a/objs/android_native_app_glue/android_native_app_glue.o: \
+  /Users/MartinRGB/Library/Android/sdk/ndk-bundle/sources/android/native_app_glue/android_native_app_glue.c \
+  /Users/MartinRGB/Library/Android/sdk/ndk-bundle/sources/android/native_app_glue/android_native_app_glue.h
+
+/Users/MartinRGB/Library/Android/sdk/ndk-bundle/sources/android/native_app_glue/android_native_app_glue.h:
